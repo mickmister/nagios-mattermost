cd0ec864102b949e6f44d5ca0f8b659c13b97ca7
mickmister <mjkochell@gmail.com>
2019-07-11 03:30:00 -0400

    Make port configurable

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -1,4 +1,10 @@
+ADD conf/ports.conf /etc/apache2/ports.conf
+ADD conf/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+
+cat /etc/apache2/ports-temp.conf | sed "s/80/${PORT}/g" > /etc/apache2/ports.conf
+cat /etc/apache2/sites-enabled/000-default-temp.conf | sed "s/80/${PORT}/g" > /etc/apache2/sites-enabled/000-default.conf
+
+cat /usr/local/nagios/etc/objects/commands-temp.cfg | sed "s#MM_URL#${MM_URL}#g" | sed "s#MM_CHANNEL#${MM_CHANNEL}#g" > /usr/local/nagios/etc/objects/commands.cfg
+
 service apache2 start
-# service nagios start
-# cat
 /usr/local/nagios/bin/nagios /usr/local/nagios/etc/nagios.cfg
